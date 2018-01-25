5fed5fc2d2e691967c673b640abb9f963828bc12
Nikolay Zhidenko <dragg.ko@gmail.com>
2018-01-25 10:52:16 +0300

    Add uniqueModel and existsModel rules

---
+++ src/Rule.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ITBrains\Validation;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rule extends \Illuminate\Validation\Rule
+{
+    /**
+     * Get a unique constraint builder instance.
+     *
+     * @param string $modelClass
+     * @param  string $column
+     * @return \Illuminate\Validation\Rules\Unique
+     */
+    public static function uniqueModel(string $modelClass, $column = 'NULL')
+    {
+        /** @var Model $instance */
+        $instance = (new $modelClass);
+
+        $uniqueRule = new \Illuminate\Validation\Rules\Unique($instance->getTable(), $column);
+
+        if (in_array(\Illuminate\Database\Eloquent\SoftDeletes::class, class_uses($modelClass))) {
+            $uniqueRule = $uniqueRule->whereNull($instance->getDeletedAtColumn());
+        }
+
+        return $uniqueRule;
+    }
+
+    /**
+     * @param string $modelClass
+     * @param string|null $column
+     * @return \Illuminate\Validation\Rules\Exists
+     */
+    public static function existsModel(string $modelClass, string $column = null)
+    {
+        /** @var Model $instance */
+        $instance = new $modelClass;
+
+        $existsRule = static::exists($instance->getTable(), $column ?: $instance->getKeyName());
+
+        if (in_array(\Illuminate\Database\Eloquent\SoftDeletes::class, class_uses($modelClass))) {
+            $existsRule = $existsRule->whereNull($instance->getDeletedAtColumn());
+        }
+
+        return $existsRule;
+    }
+}
